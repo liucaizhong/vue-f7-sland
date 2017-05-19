0222b80194d49ff217b2dcd2c85f45113fb43ca4
liucaizhong <jojoliu12345@gmail.com>
2017-05-19 08:56:28 +0800

    finish

--- src/app.vue
+++ src/app.vue
@@ -38,15 +38,18 @@
               :found="found"
             >
             </contacts>
-            <div class="contact-sorter-container" >
-              <ul class="contact-sorter">
-                <li v-for="key in sorter"><a @click="scrollTo(key)">{{key}}</a></li>
-              </ul>
-            </div>
             <f7-toolbar bottom>
               <f7-link class="btn-add" :open-popup="'#'+popupId"><f7-icon f7="add"></f7-icon></f7-link>
             </f7-toolbar>
           </f7-page>
+          <div class="contact-sorter-container" >
+            <ul class="contact-sorter"
+              @touchstart="touchStartHandler"
+              @touchmove="touchMoveHandler"
+            >
+              <li v-for="key in sorter"><a @click="scrollTo(key)">{{key}}</a></li>
+            </ul>
+          </div>
         </f7-pages>
 
       </f7-view>
@@ -80,34 +83,6 @@ export default {
       startKey: 0
     }
   },
-  mounted () {
-    let el = document.querySelector('ul.contact-sorter')
-    el.addEventListener('touchstart', (event) => {
-      if(event.targetTouches.length == 1) {
-        event.preventDefault();// 阻止浏览器默认事件，重要
-        let touch = event.targetTouches[0]
-        this.startPageY = touch.pageY
-        this.startKey = this.sorter.indexOf(touch.target.innerText)
-        console.log('start pageY is '+this.startPageY)
-        console.log('start key is '+this.startKey)
-      }
-    })
-    el.addEventListener('touchmove', (event) => {
-     // 如果这个元素的位置内只有一个手指的话
-      if(event.targetTouches.length == 1) {
-        event.preventDefault();// 阻止浏览器默认事件，重要
-        let touch = event.targetTouches[0]
-        // 把元素放在手指所在的位置
-        let curPageY = touch.pageY
-        let count = Math.floor((curPageY - this.startPageY)/16)
-        let curKey = this.startKey + count
-        let scrollTo = document.getElementById(this.sorter[curKey])
-        if(scrollTo) {
-          document.querySelector('div.page-content').scrollTop = scrollTo.offsetTop
-        }
-      }
-    }, false)
-  },
   methods: {
     onSearch (query, found) {
       this.found = found.length
@@ -124,6 +99,27 @@ export default {
       let el = document.querySelector('div.page-content')
       let scrollTo = document.getElementById(divId)
       el.scrollTop = scrollTo.offsetTop
+    },
+    touchStartHandler (event) {
+      if(event.targetTouches.length == 1) {
+        event.preventDefault();
+        let touch = event.targetTouches[0]
+        this.startPageY = touch.pageY
+        this.startKey = this.sorter.indexOf(touch.target.innerText)
+      }
+    },
+    touchMoveHandler (event) {
+      if(event.targetTouches.length == 1) {
+        event.preventDefault();
+        let touch = event.targetTouches[0]
+        let curPageY = touch.pageY
+        let count = Math.floor((curPageY - this.startPageY)/16)
+        let curKey = this.startKey + count
+        let scrollTo = document.getElementById(this.sorter[curKey])
+        if(scrollTo) {
+          document.querySelector('div.page-content').scrollTop = scrollTo.offsetTop
+        }
+      }
     }
   },
   computed: {
